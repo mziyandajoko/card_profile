aefb881a73fd634ffc54e300eefad813b061b6b0
Mziyanda Joko <joko.mzee@gmail.com>
2023-11-17 15:09:47 +0200

    adds developer image and few basic styling

--- app/components/card/page.jsx
+++ app/components/card/page.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
+import { FaLinked,FaGithub, FaInbox, FaLinkedin} from 'react-icons/fa';
 
+
+import Image from 'next/image';
 const card = () => {
   return (
-    <div className='card min-h-full min-w-full'>
+    <div className='card min-h-full overflow-clip min-w-full'>
     <div className='flex justify-between'>
-      <div className='card__contact'>
+      <div className='card__contact flex-row align-text-bottom p-5 text-white'>
         <div>
-            <h5>Email</h5>
+            <h5 className='py-2'>Email</h5>
             <p>joko.mzee@gmail.com</p>
         </div>
-        <div></div>
+        <div>
+            <h5 className='py-2'>Phone</h5>
+            <p tell="062 4204 382">062 4204 382</p>
+        </div>
+          <h5 className='py-2'>Find me on </h5>
+        <div className='flex pe-8'>
+          <FaInbox />
+          <FaGithub />
+          <FaLinkedin />
+
+
+        </div>
       </div>
       <div>
-        Image
+       <Image className='p-2' alt="developer image" src={`/images/developer.jpeg`}  width={500}
+      height={700}/>
       </div>
-      <div>
-      <h1 className='text-3xl font-semibold'>Mziyanda Joko</h1>
-      <p className='text-lg font-medium'>Front-end Developer . Cape Town</p>
+      <div className='py-5'>
+      <h1 className='text-4xl font-bold'>Mziyanda Joko</h1>
+      <p className='text-lg font-semibold'>Front-end Developer . Cape Town</p>
         <p className='text-sm'>A young dynamic individual willing to try new cutting edge technologies.A devoper who loves working in a teams,
         with good communication and leadershp skills.</p>
-      <h3 className='text-lg font-medium'>Workign with technologies:</h3>
+      <h3 className='text-lg font-semibold'>Workign with technologies:</h3>
       <ul className='text-sm'>
         <li>Flutter</li>
         <li>React</li>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Profile Card',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
