61b8aa2a5ead26bd1bf43a221982444a3b80d2c5
Mziyanda Joko <joko.mzee@gmail.com>
2023-11-29 16:02:59 +0200

    absolute position image

--- app/components/footer/page.js
+++ app/components/footer/page.js
@@ -3,7 +3,7 @@ import {FaGithub, FaLinkedin, FaInbox, } from 'react-icons/fa';
 
 function footer() {
   return (
-    <footer className='mt-4 p-5 '>
+    <footer className='mt-4 p-5 pb-16'>
         <h5 className='text-white'>Find me on </h5>
           <div className='flex justify-between w-20 pt-3 text-white'>
             <FaInbox />
